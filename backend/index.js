7849aa6307b2ccc491673983695bebc5c5311d86
tanu-shri-25 <tanushri2530@gmail.com>
2025-06-26 13:47:27 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://authentication-wine-five.vercel.app',
   credentials: true
 }));
 app.use(express.json());
